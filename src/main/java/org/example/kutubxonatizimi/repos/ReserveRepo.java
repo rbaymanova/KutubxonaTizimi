07b6fc2043c5c20eb5880cc65355eb537d23845f
Ruxshona Baymanova <rbaymanova@gmail.com>
2025-05-17 23:11:51 +0500

    ReserveRepo edited

--- src/main/java/org/example/kutubxonatizimi/repos/ReserveRepo.java
+++ src/main/java/org/example/kutubxonatizimi/repos/ReserveRepo.java
@@ -1,9 +1,15 @@
 package org.example.kutubxonatizimi.repos;
 
 import jakarta.persistence.Entity;
+import org.example.kutubxonatizimi.entities.BookStatus;
 import org.example.kutubxonatizimi.entities.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface ReserveRepo extends JpaRepository<Reservation, Long> {}
+public interface ReserveRepo extends JpaRepository<Reservation, Long> {
+
+    List<Reservation> findByStatus(BookStatus status);
+}
